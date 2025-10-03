383be6ae233843c26b2da655ae146726469bc78c
Marcos Mendes <_marcos.mendes.ext.AZ@sodexo.com>
2025-10-03 14:13:26 -0300

    Update cake display logic in effect.js and replace cake div with an image in index.html for improved presentation.

--- effect.js
+++ effect.js
@@ -188,7 +188,7 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
+					$('.cake-image').fadeIn('fast');
 				});
 				
 			}
